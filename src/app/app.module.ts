9b0498430bc0f381452df6cb69fb91d6edd178ae
jamal siam <jamalsiam1995@gmail.com>
2019-01-05 22:32:55 +0200

    restructure components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SlideshowModule} from 'ng-simple-slideshow';
+import { SlideshowModule } from 'ng-simple-slideshow';
 
+import { CardViewerComponent } from './common/UIcomponent/card/card-viewer/card-viewer.component';
+import { CardComponent } from './common/UIcomponent/card/card.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { AsideBarComponent } from './layout/aside-bar/aside-bar.component';
-import { SlideshowComponent } from './common/slideshow/slideshow.component';
+import { SlideshowComponent } from './common/UIcomponent/slideshow/slideshow.component';
 import { HomeComponent } from './component/home/home.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { FeatureBoxComponent } from './common/feature-box/feature-box.component';
-import { SearchAreaComponent } from './common/search-area/search-area.component';
-import { ItemCardComponent } from './common/item-card/item-card.component';
+import { FeatureBoxComponent } from './common/UIcomponent/feature-box/feature-box.component';
+import { SearchAreaComponent } from './common/UIcomponent/search-area/search-area.component';
+import { ItemCardComponent } from './common/UIcomponent/item-card/item-card.component';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
@@ -30,14 +32,16 @@ import { PageNotFoundComponent } from './component/page-not-found/page-not-found
     ItemCardComponent,
     LoginComponent,
     SignupComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    CardViewerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     SlideshowModule,
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
